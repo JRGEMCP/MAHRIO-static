5edff7fec59ef0128c0b52e1acafa7032b787850
emcp <emcp@whichdegree.co>
2018-08-13 20:17:14 +0200

    Latest working with a .env file in both local and docker

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 process.env.NODE_ENV="development";
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'development') {
+  require('node-env-file')('.env');
+  console.log('Running Development!');
+}
+
 require('mahrio').runServer(process.env, __dirname)
  .then( function(server) {
 
