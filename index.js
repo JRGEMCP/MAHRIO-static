b24c95e6ef832db4d92b6310ef27d7e7756bf76b
emcp <emcp@whichdegree.co>
2018-10-07 15:04:41 +0200

    Adding basic banner static site for whichdegree.co

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ if (process.env.NODE_ENV === 'development') {
 require('mahrio').runServer(process.env, __dirname)
  .then( function(server) {
 
+   server.route({
+       path: '/{param*}',
+       method: 'GET',
+       handler: {
+         directory: {
+           path: ['../public/']
+         }
+       }
+   });
+
  	server.route({
-		path: '/',
+		path: '/demo',
 		method: 'GET',
 		handler: function(req, rep){
 			rep('Your static site new here!');
